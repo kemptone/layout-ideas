dbba60348e0755b09b2a2858e5022f461e0cc1d4
justin kempton <justin@talentsky.com>
2023-03-17 13:16:56 -0700

    changing to full height, instead of fraction

--- static/messaging.js
+++ static/messaging.js
@@ -2,9 +2,14 @@ const DEBUG = false;
 
 function calculateHeight() {
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  const vh = window.innerHeight * 0.01;
+  // const vh = window.innerHeight * 0.01;
+  const vertical_height = window.innerHeight;
   // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  // document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty(
+    "--vertical-height",
+    `${vertical_height}px`,
+  );
 
   if (DEBUG) {
     // FOR TESTING IPHONE ONLY
